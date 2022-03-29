807be4fdc62a3d63e0edb7094a7514ab1ecd87ab
limmihwa <97150031+limmihwa@users.noreply.github.com>
2022-03-29 16:04:01 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// main.js
+
+$(function(){
+    // 모바일 토글 메뉴
+    $('.num-btn1').click(function(){
+       
+      $('.account-number-1').toggleClass('on'); 
+    });
+
+
+
+    $('.num-btn2').click(function(){
+       
+        $('.account-number-2').toggleClass('on'); 
+      })
+  
+  })
+
---
+++ swiper.js
@@ -0,0 +1,28 @@
+// const swiper = new Swiper('.swiper', {
+//     // 옵션
+//     direction: 'horizontal',
+//     effect: 'fade',
+//     loop: true,
+//     autoplay: {
+//         delay:7000
+//     },
+  
+//     // 하단 버튼 UI
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // 방향화살표
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
